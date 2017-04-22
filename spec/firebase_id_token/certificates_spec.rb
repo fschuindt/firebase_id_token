3bc853c24ae64d4f85b863529b94b4797518c20b
Fernando Schuindt <f.schuindtcs@gmail.com>
2017-04-22 15:10:49 -0300

    Update Certificates interface examples

--- spec/firebase_id_token/certificates_spec.rb
+++ spec/firebase_id_token/certificates_spec.rb
@@ -24,56 +24,67 @@ module FirebaseIdToken
       mock_request
     end
 
-    describe '#download' do
-      it 'requests public keys when Redis database is empty' do
+    describe '#request' do
+      it 'requests certificates when Redis database is empty' do
         expect(HTTParty).to receive(:get).
           with(FirebaseIdToken::Certificates::URL)
-        described_class.download
+        described_class.request
       end
 
-      it 'does not requests public keys when Redis database is written' do
+      it 'does not requests certificates when Redis database is written' do
         expect(HTTParty).to receive(:get).
           with(FirebaseIdToken::Certificates::URL).once
-        2.times { described_class.download }
+        2.times { described_class.request }
       end
     end
 
-    describe '#download!' do
-      it 'always requests public keys' do
+    describe '#request_anyway' do
+      it 'always requests certificates' do
         expect(HTTParty).to receive(:get).
           with(FirebaseIdToken::Certificates::URL).twice
-        2.times { described_class.download! }
+        2.times { described_class.request_anyway }
       end
 
       it 'sets the certificate expiration time as Redis TTL' do
-        described_class.download!
+        described_class.request_anyway
         expect(redis.ttl('certificates')).to be > 3600
       end
 
       it 'raises a error when certificates expires in less than 1 hour' do
         ttl_30min = (DateTime.now + (1/24r)/2).to_s
         allow(response).to receive(:headers) { { 'expires' => ttl_30min } }
-        expect{ described_class.download! }.
+        expect{ described_class.request_anyway }.
           to raise_error(Exceptions::Certificates::TimeToLiveError)
       end
     end
 
+    describe '#present?' do
+      it 'returns false when Redis database is empty' do
+        expect(described_class.present?).to be(false)
+      end
+
+      it 'returns true when Redis database is written' do
+        described_class.request
+        expect(described_class.present?).to be(true)
+      end
+    end
+
     describe '#x509' do
-      context 'before downloading certificates' do
+      context 'before requesting certificates' do
         it 'raises a error' do
           expect{ described_class.x509 }.
             to raise_error(Exceptions::Certificates::NoEntityError)
         end
       end
 
-      context 'after downloading certificates' do
+      context 'after requesting certificates' do
         it 'returns a array of hashes: String keys' do
-          described_class.download
+          described_class.request
           expect(described_class.x509.first.keys[0]).to be_a(String)
         end
 
         it 'returns a array of hashes: OpenSSL::X509::Certificate values' do
-          described_class.download
+          described_class.request
           expect(described_class.x509.first.values[0]).
             to be_a(OpenSSL::X509::Certificate)
         end
