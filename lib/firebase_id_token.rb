4120f788b15a6b4b028d746920f4d1b487efa29b
Fernando Schuindt <f.schuindtcs@gmail.com>
2017-04-23 14:15:39 -0300

    Require all 3rd party gems

--- lib/firebase_id_token.rb
+++ lib/firebase_id_token.rb
@@ -1,23 +1,19 @@
+require 'redis'
+require 'redis-namespace'
+require 'httparty'
+require 'jwt'
+
 require 'firebase_id_token/version'
 require 'firebase_id_token/exceptions/certificates/request_code_error'
 require 'firebase_id_token/exceptions/certificates/time_to_live_error'
 require 'firebase_id_token/configuration'
 require 'firebase_id_token/certificates'
-# require 'firebase_id_token/signature'
 
-# Those two may raise a error.
 # FirebaseIdToken::Certificates.request
 # FirebaseIdToken::Certificates.request_anyway
-
-# boolean
 # FirebaseIdToken::Certificates.present?
-
-# Hash of certificates or a empty hash.
 # FirebaseIdToken::Certificates.x509
-# {}
-
 # FirebaseIdToken::Signature.verify(token)
-
 module FirebaseIdToken
   class << self
     attr_writer :configuration
