a3520b9204a716b2b3ec50312258dae02b8410d2
Fernando Schuindt <f.schuindtcs@gmail.com>
2017-04-25 01:42:47 -0300

    Describe Signature

---
+++ lib/firebase_id_token/signature.rb
@@ -0,0 +1,97 @@
+module FirebaseIdToken
+  # Deals with verifying if a given Firebase ID Token is signed by one of the
+  # Google's x509 certificates that Firebase uses.
+  #
+  # Also checks if the resulting JWT payload hash matches with:
+  # + `exp` Expiration time
+  # + `iat` Issued at time
+  # + User's Firebase Project ID
+  # + Non-empty UID
+  #
+  # ## Verifying a Firebase ID Token
+  #
+  # *Be sure to configure the gem to set your Firebase Project ID and a Redis
+  # server before move any forward.*
+  #
+  # **See the README for a complete guide.**
+  #
+  # **WARNING:** Trying to verify a token without any certificate saved in
+  # Redis certificates database raises a {Exceptions::NoCertificatesError}.
+  #
+  # @example
+  #   FirebaseIdToken::Signature.verify(thrusty_token)
+  #   => {"iss"=>"https://securetoken.google.com/your-project-id", [...]}
+  #
+  #   FirebaseIdToken::Signature.verify(fake_token)
+  #   => nil
+  #
+  # @see Signature#verify
+  class Signature
+    # Pre-default JWT algorithm parameters as recommended
+    # [here](https://goo.gl/uOK5Jx).
+    JWT_DEFAULTS = { algorithm: 'RS256', verify_iat: true }
+
+    # Returns the decoded JWT hash payload of the Firebase ID Token if the
+    # signature in the token matches with one of the certificates downloaded
+    # by {FirebaseIdToken::Certificates.request}, returns `nil` otherwise.
+    #
+    # It will also return `nil` when it fails in checking if all the required
+    # JWT fields are valid, as recommended [here](https://goo.gl/yOrZZX) by
+    # Firebase oficial documentation.
+    #
+    # Note that it will raise a {Exceptions::NoCertificatesError} if the Redis
+    # certificates database is empty. Ensure to call {Certificates.request}
+    # before, ideally in a background job if you are using Rails.
+    # @return [nil, Hash]
+    def self.verify(jwt_token)
+      new(jwt_token).verify
+    end
+
+    # Loads attributes: `:project_ids` from {FirebaseIdToken::Configuration},
+    # and `:kid`, `:jwt_token` from the related `jwt_token`.
+    # @param [String] jwt_token Firebase ID Token
+    def initialize(jwt_token)
+      @project_ids = FirebaseIdToken.configuration.project_ids
+      @kid = JWT.decode(jwt_token, nil, false).last['kid']
+      @jwt_token = jwt_token
+    end
+
+    # @see Signature.verify
+    def verify
+      if cert = FirebaseIdToken::Certificates.find(@kid)
+        payload = decode_jwt_payload(@jwt_token, cert.public_key)
+        authorize payload
+      end
+    rescue JWT::VerificationError
+    end
+
+    private
+
+    def decode_jwt_payload(token, cert_key)
+      JWT.decode(token, cert_key, true, JWT_DEFAULTS).first
+    end
+
+    def authorize(payload)
+      if payload && authorized?(payload)
+        payload
+      end
+    end
+
+    def authorized?(payload)
+      still_valid?(payload) &&
+      @project_ids.include?(payload['aud']) &&
+      issuer_authorized?(payload) &&
+      ! payload['sub'].empty?
+    end
+
+    def still_valid?(payload)
+      payload['exp'].to_i > Time.now.to_i &&
+      payload['iat'].to_i < Time.now.to_i
+    end
+
+    def issuer_authorized?(payload)
+      issuers = @project_ids.map { |i| "https://securetoken.google.com/#{i}" }
+      issuers.include? payload['iss']
+    end
+  end
+end
