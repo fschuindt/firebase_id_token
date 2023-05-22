d1390644262a6e74b5dad3ff3b5f1fced1969848
Jay OConnor <jay@jayoconnor.com>
2023-05-22 14:44:55 -0700

    allow cache to be an ActiveSupport::Cache object

--- lib/firebase_id_token/configuration.rb
+++ lib/firebase_id_token/configuration.rb
@@ -4,11 +4,15 @@ module FirebaseIdToken
   LIB_PATH = File.expand_path('../../', __FILE__)
 
   class Configuration
-    attr_accessor :redis, :project_ids, :certificates
+    attr_accessor :redis, :project_ids, :certificates, :cache_store
 
     def initialize
       @project_ids = []
       @certificates = FirebaseIdToken::Certificates
+      # support older API where redis attribute was explictly set
+      if redis
+        @cache_store = ActiveSupport::Cache.RedisStore.new(redis)
+      end
     end
   end
 end
